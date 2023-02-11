da1bb0cf610bc4d9dfbd5e1599b02c2f0cba2e0d
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-02-11 16:24:25 +0100

    Restrict User editing to oneself

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -9,6 +9,11 @@ use Illuminate\Validation\Rules\Password;
 
 class UpdateUserRequest extends FormRequest
 {
+    public function authorize(Request $request): bool
+    {
+        return $request->route()->parameter('user')->is(me());
+    }
+
     /**
      * Get the validation rules that apply to the request.
      */
@@ -17,10 +22,11 @@ class UpdateUserRequest extends FormRequest
         $affectedUser = $request->route()->parameter('user');
 
         return [
-            'name'         => 'alpha|max:255',
-            'email'        => [ 'email', 'max:255', Rule::unique('users')->ignore($affectedUser) ],
-            'password'     => [ 'confirmed', Password::defaults() ],
-            'old_password' => 'required_with:password|current_password',
+            'name'          => 'alpha|max:255',
+            'email'         => [ 'email', 'max:255', Rule::unique('users')->ignore($affectedUser) ],
+            'password'      => [ 'confirmed', Password::defaults() ],
+            'old_password'  => 'required_with:password|current_password',
+            'active_course' => 'exists:courses,id',
         ];
     }
 }
