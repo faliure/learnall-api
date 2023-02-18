d2675f693944908ae5d1a8d0306b409a019c9afd
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-02-18 20:02:12 +0100

    Refactor core seeding

--- database/migrations/2023_02_08_190911_seed_core_data.php
+++ database/migrations/2023_02_08_190911_seed_core_data.php
@@ -4,6 +4,7 @@ use App\Models\Course;
 use App\Models\ExerciseType;
 use App\Models\Language;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\File;
 
 return new class extends Migration
 {
@@ -23,273 +24,40 @@ return new class extends Migration
 
     private function seedLanguages(): void
     {
-        Language::upsert([
-            [
-                'code'    => 'en',
-                'name'    => 'English',
-                'region'  => null,
-                'flag'    => 'US',
-            ],
-            [
-                'code'    => 'es',
-                'name'    => 'Spanish',
-                'region'  => 'Spain',
-                'flag'    => 'ES',
-            ],
-            [
-                'code'    => 'fr',
-                'name'    => 'French',
-                'region'  => null,
-                'flag'    => 'FR',
-            ],
-            [
-                'code'    => 'pt',
-                'name'    => 'Portuguese',
-                'region'  => 'Brazil',
-                'flag'    => 'BR',
-            ],
-            [
-                'code'    => 'it',
-                'name'    => 'Italian',
-                'region'  => null,
-                'flag'    => 'IT',
-            ],
-            [
-                'code'    => 'uk',
-                'name'    => 'Ukrainian',
-                'region'  => null,
-                'flag'    => 'UA',
-            ],
-            [
-                'code'    => 'ro',
-                'name'    => 'Romanian',
-                'region'  => null,
-                'flag'    => 'RO',
-            ],
-            [
-                'code'    => 'la',
-                'name'    => 'Latin',
-                'region'  => null,
-                'flag'    => 'VA',
-            ],
-            [
-                'code'    => 'cs',
-                'name'    => 'Czech',
-                'region'  => null,
-                'flag'    => 'CZ',
-            ],
-            [
-                'code'    => 'ru',
-                'name'    => 'Russian',
-                'region'  => null,
-                'flag'    => 'RU',
-            ],
-            [
-                'code'    => 'ja',
-                'name'    => 'Japanese',
-                'region'  => null,
-                'flag'    => 'JP',
-            ],
-            [
-                'code'    => 'nl',
-                'name'    => 'Dutch',
-                'region'  => null,
-                'flag'    => 'NL',
-            ],
-            [
-                'code'    => 'de',
-                'name'    => 'German',
-                'region'  => null,
-                'flag'    => 'DE',
-            ],
-            [
-                'code'    => 'sv',
-                'name'    => 'Swedish',
-                'region'  => null,
-                'flag'    => 'SE',
-            ],
-            [
-                'code'    => 'ca',
-                'name'    => 'Catalan',
-                'region'  => null,
-                'flag'    => null, // No flag in the sprite... TODO
-            ],
-            [
-                'code'    => 'eo',
-                'name'    => 'Esperanto',
-                'region'  => null,
-                'flag'    => null, // No flag in the sprite... TODO
-            ],
-            [
-                'code'    => 'hu',
-                'name'    => 'Hungarian',
-                'region'  => null,
-                'flag'    => 'HU',
-            ],
-            [
-                'code'    => 'pl',
-                'name'    => 'Polish',
-                'region'  => null,
-                'flag'    => 'PL',
-            ],
-        ], [ 'code' ], [ 'name', 'region' ]);
+        $languages = readJsonFile(database_path('data/languages.json'));
+
+        Language::upsert($languages, [ 'code' ], [ 'name' ]);
     }
 
     public function seedCourses(): void
     {
-        Language::pluck('id', 'code')->each(function ($fromLanguage) {
-            Language::pluck('id', 'code')->each(function ($language) use ($fromLanguage) {
-                if ($fromLanguage !== $language) {
-                    Course::create([
-                        'from_language' => $fromLanguage,
-                        'language_id'   => $language,
-                        'enabled'       => false,
-                    ]);
-                }
-            });
-        });
+        $languages = Language::pluck('id', 'code');
 
-        collect([
+        $enabled = [
             ['en', 'es'],
-            ['en', 'fr'],
-            ['en', 'pt'],
-            ['en', 'it'],
             ['en', 'uk'],
-            ['en', 'de'],
             ['es', 'en'],
-            ['es', 'fr'],
-            ['es', 'pt'],
             ['es', 'uk'],
-            ['it', 'en'],
-            ['fr', 'en'],
-            ['pt', 'en'],
-            ['uk', 'en'],
-            ['uk', 'es'],
-        ])->each(function ($toFrom) {
-            [ $to, $from ] = $toFrom;
+        ];
 
-            Course::withoutGlobalScopes()
-                ->whereHas('fromLanguage', fn ($q) => $q->where('code', $from))
-                ->whereHas('language', fn ($q) => $q->where('code', $to))
-                ->update(['enabled' => true]);
-        });
+        $courses = collect(File::glob(database_path('data/duome/*.json')))
+            ->map(fn (string $path) => explode('-', basename($path, '.json')))
+            ->map(fn (array $pair) => [
+                'from_language' => $languages[ $pair[0] ],
+                'language_id'   => $languages[ $pair[1] ],
+                'enabled'       => in_array($pair, $enabled),
+            ])
+            ->toArray();
+
+        Course::upsert($courses, [ 'language_id', 'from_language' ], [ 'enabled' ]);
     }
 
     private function seedExerciseTypes()
     {
-        ExerciseType::upsert([
-            [
-                'type'                     => 'ReadAndTranslate',
-                'requires_listening'       => false,
-                'requires_speaking'        => false,
-                'requires_target_keyboard' => true,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'Translate this sentence',
-            ],
-            [
-                'type'                     => 'ReadAndTranslateBack',
-                'requires_listening'       => false,
-                'requires_speaking'        => false,
-                'requires_target_keyboard' => false,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'What does this mean?',
-            ],
-            [
-                'type'                     => 'ReadAndPickTheWords',
-                'requires_listening'       => false,
-                'requires_speaking'        => false,
-                'requires_target_keyboard' => false,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'Translate this sentence',
-            ],
-            [
-                'type'                     => 'ReadAndPickTheWordsBack',
-                'requires_listening'       => false,
-                'requires_speaking'        => false,
-                'requires_target_keyboard' => false,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'What does this mean?',
-            ],
-            [
-                'type'                     => 'ReadAndSpeak',
-                'requires_listening'       => false,
-                'requires_speaking'        => true,
-                'requires_target_keyboard' => false,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'Repeat this',
-            ],
-            [
-                'type'                     => 'ReadAndSpeakTranslation',
-                'requires_listening'       => false,
-                'requires_speaking'        => true,
-                'requires_target_keyboard' => false,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'Repeat this',
-            ],
-            [
-                'type'                     => 'ListenAndWrite',
-                'requires_listening'       => true,
-                'requires_speaking'        => false,
-                'requires_target_keyboard' => true,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'What do you hear?',
-            ],
-            [
-                'type'                     => 'ListenAndTranslate',
-                'requires_listening'       => true,
-                'requires_speaking'        => false,
-                'requires_target_keyboard' => false,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'Translate what you hear',
-            ],
-            [
-                'type'                     => 'ListenAndPickTheWords',
-                'requires_listening'       => true,
-                'requires_speaking'        => false,
-                'requires_target_keyboard' => false,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'What do you hear?',
-            ],
-            [
-                'type'                     => 'ListenAndPickTheWordsBack',
-                'requires_listening'       => true,
-                'requires_speaking'        => false,
-                'requires_target_keyboard' => false,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'What do you hear?',
-            ],
-            [
-                'type'                     => 'ListenAndRepeat',
-                'requires_listening'       => true,
-                'requires_speaking'        => true,
-                'requires_target_keyboard' => false,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'Repeat what you hear',
-            ],
-            [
-                'type'                     => 'SelectImageOption',
-                'requires_listening'       => false,
-                'requires_speaking'        => false,
-                'requires_target_keyboard' => false,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => 'What is the correct translation?',
-            ],
-            [
-                'type'                     => 'TranslateWithImage',
-                'requires_listening'       => false,
-                'requires_speaking'        => false,
-                'requires_target_keyboard' => false,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => '',
-            ],
-            [
-                'type'                     => 'TranslateWithImageBack',
-                'requires_listening'       => false,
-                'requires_speaking'        => false,
-                'requires_target_keyboard' => true,
-                'spec'                     => json_encode([]), // TODO
-                'description'              => '',
-            ],
-        ], ['type'], ['requires_listening', 'requires_speaking', 'requires_target_keyboard', 'description', 'spec']);
+        ExerciseType::upsert(
+            readJsonFile(database_path('data/exerciseTypes.json')),
+            [ 'type' ],
+            [ 'requires_listening', 'requires_speaking', 'requires_target_keyboard', 'description', 'spec' ]
+        );
     }
 };
