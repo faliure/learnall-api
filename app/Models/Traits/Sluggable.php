0d5669e611f0251f873771b5a596137cb01bbf77
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-02-19 16:29:35 +0100

    Refactor Sluggable Model Trait

--- app/Models/Traits/Sluggable.php
+++ app/Models/Traits/Sluggable.php
@@ -2,16 +2,12 @@
 
 namespace App\Models\Traits;
 
+use BadMethodCallException;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 trait Sluggable
 {
-    /**
-     * The attribute of the model that the slug is based of.
-     */
-    protected string $sluggableAttribute = 'name';
-
     public static function bootSluggable()
     {
         static::creating(function (Model $sluggable) {
@@ -19,7 +15,18 @@ trait Sluggable
                 return;
             }
 
-            $sluggable->slug = Str::slug($sluggable->{$sluggable->sluggableAttribute});
+            $sluggable->slug = Str::slug($sluggable->sluggable());
         });
+    }
+
+    protected function sluggable(): string
+    {
+        if (! is_string($this->name)) {
+            throw new BadMethodCallException(
+                'Cannot find a valid sluggable property for ' . class_basename($this)
+            );
+        }
+
+        return $this->name;
     }
 }
